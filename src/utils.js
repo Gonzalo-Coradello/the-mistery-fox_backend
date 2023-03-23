c63eb32595205f1aacc4ff670960ef78a6c503e1
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-03-22 23:55:07 -0300

    create auto increment code generator

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,9 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import passport from "passport";
 import config from "./config/config.js";
+import { ticketsService } from "./repositories/index.js";
+import TicketModel from "./dao/models/ticket.model.js";
+import ProductModel from "./dao/models/product.model.js";
 
 const { PRIVATE_KEY } = config;
 
@@ -50,8 +53,6 @@ export const authorization = (role) => {
   return async (req, res, next) => {
     const user = req.user || null;
 
-    console.log(req.user)
-
     if (!user)
       return res
         .status(401)
@@ -88,6 +89,26 @@ export const createHash = (password) =>
   bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 export const isValidPassword = (user, password) =>
   bcrypt.compareSync(password, user.password);
+
+export const codeGenerator = async () => {
+  const last = (await TicketModel.find().sort({ purchase_datetime: -1 }))[0].code
+  
+  let letters = last.slice(0, 2)
+  let nums = parseInt(last.slice(2))
+
+  if(nums === 99) {
+    if (letters.charCodeAt(1) === 90) {
+      letters = String.fromCharCode((letters.charCodeAt(0) + 1)).concat(String.fromCharCode(65))
+    } else {
+      letters = letters[0].concat(String.fromCharCode((letters.charCodeAt(1) + 1)))
+    }
+    nums = "00"
+  } else {
+    nums = (nums + 1).toLocaleString(undefined, {minimumIntegerDigits: 2})
+  }
+
+  return letters.concat(nums)
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
