126f51dc62d01a3af8475864fc1ebbc59f1c1cf9
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-05-11 10:01:33 -0300

    fix mongoose update error

--- src/dao/classes/carts.dao.js
+++ src/dao/classes/carts.dao.js
@@ -1,18 +1,21 @@
 import CartModel from "../models/cart.model.js";
 
 export default class Cart {
-    constructor() {}
+  constructor() {}
 
-    create = async () => {
-        const cart = await CartModel.create({ products: [] })
-        return cart
-    }
+  create = async () => {
+    const cart = await CartModel.create({ products: [] });
+    return cart;
+  };
 
-    getByID = async (id) => {
-        return await CartModel.findById(id).populate("products.product")
-    }
+  getByID = async (id) => {
+    return await CartModel.findById(id).populate({ path: "products.product", model: "Products" });
+  };
 
-    update = async (id, data) => {
-        return await CartModel.updateOne({ _id: id }, data)
-    }
+  update = async (id, data) => {
+    return await CartModel.updateOne(
+      { _id: id },
+      { $set: { products: data.products, quantity: data.quantity } }
+    );
+  };
 }
